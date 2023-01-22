a27c210ff9d9962af548e1c55f5e33cb5ba1f6fc
Joey Wills <jdwills95+github@gmail.com>
2023-01-21 21:45:48 -0600

    Adding api.

---
+++ main.py
@@ -0,0 +1,58 @@
+# main.py
+
+import uvicorn
+from fastapi import FastAPI
+from datetime import datetime
+from get_internet_health_database import get_database
+from pydantic import BaseModel
+
+dbname = get_database()
+collection_ping = dbname["ping_data"]
+collection_speed = dbname["speed_data"]
+
+
+class Ping(BaseModel):
+    date_time: datetime
+    ip: str
+    packets_received: str
+    packets_sent: str
+    latency: str
+
+
+class Speed(BaseModel):
+    date_time: datetime
+    speed_down: float
+    speed_up: float
+
+
+app = FastAPI()
+
+
+def __aiter__(self):
+    return self
+
+
+@app.get("/ping/{from_date}/{to_date}")
+def root(from_date: str, to_date: str):
+    f_from_date = datetime.fromisoformat(from_date)
+    f_to_date = datetime.fromisoformat(to_date)
+    ping_data = collection_ping.find({'date_time': {'$gte': f_from_date, '$lt': f_to_date}})
+    pings = []
+    for ping in ping_data:
+        pings.append(Ping(**ping))
+    return pings
+
+
+@app.get("/speedtest/{from_date}/{to_date}")
+def root(from_date: str, to_date: str):
+    f_from_date = datetime.fromisoformat(from_date)
+    f_to_date = datetime.fromisoformat(to_date)
+    speed_data = collection_speed.find({'date_time': {'$gte': f_from_date, '$lt': f_to_date}})
+    speeds = []
+    for speed in speed_data:
+        speeds.append(Speed(**speed))
+    return speeds
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 # Internet Health Logger
-Start mongodb instance locally where the script is located.
-Run the internet_health_logger.py script.
+1. Run the following in the dir where main.py is located 'uvicorn main:app --reload'
+2. Start mongodb instance locally where the script is located.
+3. Run the internet_health_logger.py script.
+
+API docs can be found at http://127.0.0.1:8000/docs
